29d03cfb8b232d247d2a9822e165dd48175632ba
Concrete358 <94567550+Concrete358@users.noreply.github.com>
2022-05-03 19:26:17 +0200

    Delete java directory

--- java/command/ICommand.java
+++
@@ -1,9 +0,0 @@
-package command;
-
-import javax.servlet.http.HttpServletRequest;
-import command.page.AbstractPage;
-
-
-public interface ICommand {
-	AbstractPage execute(HttpServletRequest request);
-}
